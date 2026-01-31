ace4cdace6b52464b8d07af37da122eb89f58360
Tecnobyte360 <comercial@tecnobyte360.com>
2026-01-31 14:54:37 -0500

    prueba volkvox

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,36 @@
 <?php
 
-use App\Http\Controllers\Api\ApisIA\PoliticasLogisticas;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\ApisIA\PoliticasLogisticas;
 use App\Http\Controllers\Apisdoblamos\Autenticacion\LoginSanctumController;
 use App\Http\Controllers\Apisdoblamos\Autenticacion\UsuariosController;
 use App\Http\Controllers\Api\SAPDoblamos\InventarioDisponibleController;
 use App\Http\Controllers\WolkvoxWebhookController;
 
+/*
+|--------------------------------------------------------------------------
+| RUTAS PÚBLICAS
+|--------------------------------------------------------------------------
+*/
+
+// Auth
 Route::post('/login', [LoginSanctumController::class, 'login']);
-Route::post('/logout', [LoginSanctumController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/wolkvox/webhook', [WolkvoxWebhookController::class, 'handle']);
 
+// 👉 ESTA ES LA QUE USAS EN "LINK DE INTEGRACIÓN" DE WOLKVOX
+Route::get('/wolkvox/view', [WolkvoxWebhookController::class, 'handle']);
 
+/*
+|--------------------------------------------------------------------------
+| RUTAS PROTEGIDAS
+|--------------------------------------------------------------------------
+*/
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::get('/user', fn(Request $request) => $request->user());
+    Route::post('/logout', [LoginSanctumController::class, 'logout']);
+
+    Route::get('/user', fn (Request $request) => $request->user());
 
     // Usuarios
     Route::get('/usuarios', [UsuariosController::class, 'index']);
@@ -24,8 +38,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/usuarios/{id}', [UsuariosController::class, 'update']);
     Route::patch('/usuarios/{id}/password', [UsuariosController::class, 'updatePassword']);
     Route::patch('/usuarios/{id}/estado', [UsuariosController::class, 'toggleEstado']);
+
+    // SAP
     Route::get('/sap/inventario-disponible', [InventarioDisponibleController::class, 'index']);
     Route::get('/sap/inventario-disponible/query', [InventarioDisponibleController::class, 'query']);
+
+    // IA
     Route::get('/ia/politicasVentas', [PoliticasLogisticas::class, 'query']);
 });
-
